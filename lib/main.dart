0557289c400d27192bf6b5aaad6ba18c0b22ae51
Danilo Camargo Ferreira Rocha <danilo.rocha@portoseguro.com.br>
2022-04-07 09:09:11 -0300

    Curso 01: módulo 03

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,93 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Transferências'),
-        ),
-        body: const ListaTransferencia(),
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () => '',
-        ),
-      ),
-      debugShowCheckedModeBanner: false,
-    ));
+void main() => runApp(const ByteBank());
 
-// StatefulWidget => permite alterações dinâmicas
-// StatelessWidget => widget estático
-
-// REFACTORE 02
-class ListaTransferencia extends StatelessWidget {
-  const ListaTransferencia({Key? key}) : super(key: key);
+class ByteBank extends StatelessWidget {
+  const ByteBank({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const <Widget>[
-        ItemTransferencia(
-            transferencia: Transferencia(valor: 150.0, conta: 'Itaú')),
-        ItemTransferencia(
-            transferencia: Transferencia(valor: 260.0, conta: 'nuBank')),
-        ItemTransferencia(
-            transferencia: Transferencia(valor: 370.0, conta: 'Inter')),
-      ],
+    return const MaterialApp(
+      home: Formulario(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class ItemTransferencia extends StatelessWidget {
-  const ItemTransferencia({
-    Key? key,
-    required this.transferencia,
-  }) : super(key: key);
+class Formulario extends StatefulWidget {
+  const Formulario({Key? key}) : super(key: key);
 
-  final Transferencia transferencia;
+  @override
+  State<Formulario> createState() => _FormularioState();
+}
+
+class _FormularioState extends State<Formulario> {
+  final TextEditingController _controllerConta = TextEditingController();
+  final TextEditingController _controllerValor = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        leading: const Icon(Icons.monetization_on),
-        title: Text(transferencia.valor.toString()),
-        subtitle: Text(transferencia.conta),
-        trailing: const Icon(Icons.more_vert),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Criando Transferência'),
+        ),
+        body: Column(
+          children: <Widget>[
+            // INPUT CONTA
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12.0, 24.0, 12.0, 12.0),
+              child: TextField(
+                controller: _controllerConta,
+                keyboardType: const TextInputType.numberWithOptions(),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Número da conta',
+                ),
+              ),
+            ),
+            // INPUT VALOR
+            Padding(
+              padding: const EdgeInsets.fromLTRB(12.0, 12.0, 12.0, 16.0),
+              child: TextField(
+                controller: _controllerValor,
+                keyboardType: const TextInputType.numberWithOptions(),
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.monetization_on),
+                  border: OutlineInputBorder(),
+                  labelText: 'Valor',
+                ),
+              ),
+            ),
+            // BTN CONFIRMAR
+            ElevatedButton(
+              onPressed: () {
+                // debugPrint(_controllerConta.text);
+                // debugPrint(_controllerValor.text);
+                final int? conta = int.tryParse(_controllerConta.text);
+                final double? valor = double.tryParse(_controllerValor.text);
+                if (conta != null && valor != null) {
+                  final requisicao = Transferencia(conta: conta, valor: valor);
+                  debugPrint('$requisicao');
+                }
+              },
+              child: const Text('Confirmar'),
+            ),
+          ],
+        ));
   }
 }
 
 class Transferencia {
-  final double valor;
-  final String conta;
+  final int? conta;
+  final double? valor;
 
   const Transferencia({
-    required this.valor,
-    required this.conta,
+    this.conta,
+    this.valor,
   });
+
+  @override
+  String toString() {
+    return 'Transferencia: { conta: $conta , valor: $valor}';
+  }
 }
